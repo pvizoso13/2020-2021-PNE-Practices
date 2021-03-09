cec98f370598520363a106b5966280c4962c4989
pvizoso13 <p.vizoso.2019@alumnos.urjc.es>
2021-03-09 13:01:46 +0100

    fibonacci 11

--- Session-02/fibonacci.py
+++ Session-02/fibonacci.py
@@ -1,17 +1,10 @@
 n = 11
 a = 0
 b = 1
-if n < 0:
-    print("Incorrect input")
-elif n == 0:
-    print("0")
-elif n == 1:
-    print("1")
-else:
-    print(a)
-    print(b)
-    for i in range(2, n):
-        c = a + b
-        a = b
-        b = c
-        print(b)
+
+print(a, b, end=' ')
+for i in range(2, n):
+    c = a + b
+    a = b
+    b = c
+    print(b, end=' ')
