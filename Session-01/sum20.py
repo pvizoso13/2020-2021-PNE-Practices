e9ca2d2bc5546b7ba65eaa2ae02c157699e2ccdf
pvizoso13 <p.vizoso.2019@alumnos.urjc.es>
2021-03-09 11:10:26 +0100

    sum20 debugged

--- Session-01/sum20.py
+++ Session-01/sum20.py
@@ -1,6 +1,12 @@
-i = 1
-addition = 0
-while i <= 20:
-    addition = addition + i
-    i += 1
-print(addition)
+# Session 1. Exercise 3
+# Calculate the sum of the 20 first integer numbers
+# 1 + 2 + 3 + ... + 20
+# Example working!
+
+# -- Store the result
+res = 0
+
+for i in range(1, 21):
+    res += i
+
+print("Total sum: ", res)
