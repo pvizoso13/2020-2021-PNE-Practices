2ae38d122886ca70f6274f46d343943bc8fafccf
pvizoso13 <p.vizoso.2019@alumnos.urjc.es>
2021-03-10 11:29:17 +0100

    fibonacci sum n

--- Session-02/fibo-sumN.py
+++ Session-02/fibo-sumN.py
@@ -1,4 +1,20 @@
 def fibosum(n):
+    addition = 0
+    a = 0  # first element series
+    b = 1  # second element series
+    if n < 0:
+        return "Incorrect format!"
+    elif n == 1:
+        return a
+    elif n == 2:
+        return a + b
+    else:
+        for i in range(2, n+1):
+            c = a + b
+            a = b
+            b = c
+            addition = addition + b
+        return addition
 
 
 print("Sum of the First 5 terms of the Fibonacci terms: ", fibosum(5))
