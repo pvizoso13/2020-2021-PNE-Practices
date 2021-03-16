abb84b1d21f956c02c5adb7b3fab2d745daaaa81
pvizoso13 <p.vizoso.2019@alumnos.urjc.es>
2021-03-16 20:35:10 +0100

    body file

--- Session-04/body.py
+++ Session-04/body.py
@@ -1,9 +1,10 @@
 from pathlib import Path
 
-FILENAME = "RNU6_269P.txt"
+FILENAME = "U5.txt"
 
 file_contents = Path(FILENAME).read_text()
 
 index_start = file_contents.find("\n")
 seq_dna = file_contents[index_start+1:]
+print("Body of the U5.txt file: ")
 print(seq_dna)
