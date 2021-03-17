0e41fbad2eebea17a137f0c895b7f34ff4a0601f
pvizoso13 <p.vizoso.2019@alumnos.urjc.es>
2021-03-17 13:25:14 +0100

    correction

--- P0/Seq0.py
+++ P0/Seq0.py
@@ -14,8 +14,7 @@ def seq_len(filename):
     from pathlib import Path
     file_contents = Path(filename).read_text()
     index_start = file_contents.find("\n")
-    seq_dna = file_contents[index_start + 1:]
-    seq_dna = seq_dna.replace("\n", "")
+    seq_dna = file_contents[index_start + 1:].replace("\n", "")
     return len(seq_dna)
 
 
