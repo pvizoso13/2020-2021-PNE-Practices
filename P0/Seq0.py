a177b9fece9462c8bebe50cc30197cfcc6d66345
pvizoso13 <p.vizoso.2019@alumnos.urjc.es>
2021-03-17 13:22:49 +0100

    correction

--- P0/Seq0.py
+++ P0/Seq0.py
@@ -6,8 +6,7 @@ def seq_read_fasta(filename):
     from pathlib import Path
     file_contents = Path(filename).read_text()
     index_start = file_contents.find("\n")
-    seq_dna = file_contents[index_start + 1:]
-    seq_dna = seq_dna.replace("\n", "")
+    seq_dna = file_contents[index_start+1:].replace("\n", "")
     return seq_dna
 
 
