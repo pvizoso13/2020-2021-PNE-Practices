49638a400317b0ad0b728d5d632c066ee8f3aac9
pvizoso13 <p.vizoso.2019@alumnos.urjc.es>
2021-03-17 14:43:07 +0100

    SEQ-01 next is __str__ method

---
+++ Session-06/seq-01.py
@@ -0,0 +1,12 @@
+class Seq:
+    """A class for representing sequences"""
+    def __init__(self, strbases):
+        self.strbases = strbases
+        print("New sequence created!")
+
+# Main program
+
+
+s1 = Seq("AGTACACTGGT")
+s2 = Seq("CGTAAC")
+
