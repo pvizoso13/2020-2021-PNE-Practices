2d2b4d3f05a13b63b1450969fa7de260d37b1e24
pvizoso13 <p.vizoso.2019@alumnos.urjc.es>
2021-05-26 11:35:40 +0200

    Final project

--- Final project/server.py
+++ Final project/server.py
@@ -1,10 +1,25 @@
 import http.client
 import json
 
+def species_get(endpoint):
+    PORT = 8080
+    SERVER = "rest.ensembl.org"
+    print(f"\nConnecting to server: {SERVER}:{PORT}\n")
+    connection = http.client.HTTPConnection(SERVER)
+    try:
+        connection.request("GET", ENDPOINT + PARAMS)
+    except ConnectionRefusedError:
+        print("ERROR! The database is down")
+        exit()
+    response = connection.getresponse()
+    print(f"Response received!: {response.status} {response.reason}\n")
+    data = response.read().decode("utf-8")
+    data_1 = json.loads(data)
+    return data_1
+
 SERVER = "rest.ensembl.org"
 PARAMS = "?content-type=application/json"
 
-connection = http.client.HTTPConnection(SERVER)
 
 
 class TestHandler(http.server.BaseHTTPRequestHandler):
@@ -17,4 +32,6 @@ class TestHandler(http.server.BaseHTTPRequestHandler):
         if first_argument == "/":
             contents = Path('form-4.html').read_text()
             error_code = 200
-
+        elif first_argument == "/listSpecies":
+            ENDPOINT = "/info/species"
+            species =
